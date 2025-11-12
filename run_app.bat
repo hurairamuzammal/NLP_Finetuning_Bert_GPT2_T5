49d63497eb7f69452c115b309388827102dbbb50
Muhammad Abu Huraira <huraira.eqeel@gmail.com>
2025-11-12 13:19:24 +0500

    few updates

--- run_app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install -r requirements.txt
-
-echo.
-echo Make sure your model files are in: ./bert_sentiment/final/
-echo.
-echo Starting Streamlit app...
-streamlit run app.py
